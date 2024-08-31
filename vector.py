fcb6d125e47c3166daf857dc1d5031de60259b18
dipanshuagrawal987 <157246177+dipanshuagrawal987@users.noreply.github.com>
2024-09-01 04:20:18 +0530

    Create vector.py

---
+++ vector.py
@@ -0,0 +1,12 @@
+def vectorize(front_array):
+    categories = ['AI/ML', 'Remote Sensing', 'IoT', 'Embedded Systems', 'Data Analysis', 'Data Science',
+                  'Environmental Science', 'Blockchain', 'Security', 'Medical Imaging', 'Robotics', 'Game Development',
+                  '3D Modeling']
+    init_vector = [0] * len(categories)
+
+    for element in front_array:
+        if element in categories:
+            index = categories.index(element)
+            init_vector[index] = 1
+
+    return init_vector
