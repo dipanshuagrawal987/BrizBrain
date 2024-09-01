4f52df10694598a81f08a1897cabae05faacbea1
Titli06 <159781199+Titli06@users.noreply.github.com>
2024-09-01 11:10:47 +0530

    Delete projects.js

--- projects.js
+++
@@ -1,8 +0,0 @@
-// script.js
-
-document.querySelectorAll('.card').forEach(card => {
-    card.addEventListener('click', () => {
-        // Open the new page (this is just a placeholder URL)
-        window.location.href = 'newpage.html';
-    });
-});
